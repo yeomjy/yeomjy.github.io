2762fb402938ca9d96707e0e3a50c48c4ea8522b
Junyoung Yeom <yeomjy111@gmail.com>
2022-12-24 23:13:24 +0900

    dev

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -4,7 +4,29 @@ import TA from "@/components/TAExperience.vue"
 import About from "@/components/MyInfo.vue"
 </script>
 <template>
-  <About />
-  <Skills />
-  <TA />
+  <div class="container">
+  <div class="element">
+    <About />
+  </div>
+  <div class="element">
+    <Skills />
+  </div>
+  <div class="element">
+    <TA />
+  </div>
+  </div>
 </template>
+
+<style>
+.element {
+  border:5px solid;
+  padding:20px;
+  margin:20px;
+  display:block;
+  width:80vw;
+}
+.container {
+  display: grid;
+  grid-template-columns: 100vw;
+}
+</style>
