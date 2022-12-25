ec1f7ea22e288fe6bdb6550535f77e4a0fa25b29
Junyoung Yeom <yeomjy111@gmail.com>
2022-12-25 15:05:55 +0900

    update

--- src/components/MyInfo.vue
+++ src/components/MyInfo.vue
@@ -4,11 +4,13 @@
     <div>
       My name is Junyoung Yeom. I am an undergraduate student majoring in
       Computer Science and Engineering, at Seoul National University. I am also
-      double majoring in mathematics. I am interested in theory of deep
-      learning, optimization, graph neural network, and reinforcement learning.
+      double majoring in mathematics. I am interested in generalization of deep
+      neural network, optimization, graph neural network, and reinforcement
+      learning.<br />
+      I am a member of <a href="https://club-gauss.github.io">Club Gauss</a>, an
+      undergraduate student club in SNU.
     </div>
   </div>
 </template>
 
-<style>
-</style>
+<style></style>
