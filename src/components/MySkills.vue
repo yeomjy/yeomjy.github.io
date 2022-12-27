968d71e95860d897a70f354b06f422fa42eebb78
Junyoung Yeom <yeomjy111@gmail.com>
2022-12-27 19:03:30 +0900

    update

--- src/components/MySkills.vue
+++ src/components/MySkills.vue
@@ -5,7 +5,9 @@
       <v-list density="compact">
         <v-list-item>
           <v-list-item-title>PyTorch</v-list-item-title>
-          4/5
+          4/5 <br />
+          Implemented various models in research papers, and implemented my own
+          model for mini research project.
         </v-list-item>
         <v-list-item>
           <v-list-item-title>Tensorflow</v-list-item-title>
@@ -13,7 +15,15 @@
         </v-list-item>
         <v-list-item>
           <v-list-item-title>Django</v-list-item-title>
-          4/5
+          4/5 <br />
+          Developed and deployed fully working backend api service twice, with
+          async task queue, scheduled jobs, and machine learning feature.
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-title>Google Cloud Platform</v-list-item-title>
+          4/5 <br />
+          Deployment on Compute Engine and Cloud Run, and integration with Cloud
+          SQL and Cloud Storage & Cloud CDN service.
         </v-list-item>
       </v-list>
     </div>
