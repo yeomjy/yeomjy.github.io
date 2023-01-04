de700d4800ce802b82c117949d6b46f2ea5eb158
Junyoung Yeom <yeomjy111@gmail.com>
2023-01-04 19:33:15 +0900

    update

--- src/App.vue
+++ src/App.vue
@@ -5,43 +5,41 @@ import AboutView from "./views/AboutView.vue"
 <template>
   <v-app>
     <v-main>
-      <v-content>
-        <v-container>
-          <v-row dense>
-            <v-col>
-              <v-avatar size="125" class="profile">
-                <img
-                  alt="Profile Image"
-                  src="@/assets/profile.jpeg"
-                  width="125"
-                  height="125"
-                />
-              </v-avatar>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col>
-              <h1 class="name">Junyoung Yeom</h1>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col>
-              <div class="wrapper">
-                <nav>
-                  <a href="https://github.com/yeomjy">GitHub</a>
-                  <a href="mailto:yeomjy111@gmail.com">Email</a>
-                  <a href="https://yeomjy.com/blog">Blog</a>
-                </nav>
-              </div>
-            </v-col>
-          </v-row>
-          <v-row>
-            <AboutView />
-          </v-row>
-        </v-container>
-      </v-content>
+      <v-container>
+        <v-row dense>
+          <v-col>
+            <v-avatar size="125" class="profile">
+              <img
+                alt="Profile Image"
+                src="@/assets/profile.jpeg"
+                width="125"
+                height="125"
+              />
+            </v-avatar>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <h1 class="name">Junyoung Yeom</h1>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <div class="wrapper">
+              <nav>
+                <a href="https://github.com/yeomjy">GitHub</a>
+                <a href="mailto:yeomjy111@gmail.com">Email</a>
+                <a href="https://yeomjy.com/blog">Blog</a>
+              </nav>
+            </div>
+          </v-col>
+        </v-row>
+        <v-row>
+          <AboutView />
+        </v-row>
+      </v-container>
     </v-main>
-    <v-footer padless>
+    <v-footer>
       This page is built with &nbsp;
       <a href="https://vuejs.org/">Vue.js</a> &nbsp; and &nbsp;
       <a href="https://next.vuetifyjs.com/en/">Vuetify</a>
@@ -66,14 +64,6 @@ nav {
   margin-top: 2rem;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
 nav a {
   display: inline-block;
   padding: 0 1rem;
@@ -84,31 +74,4 @@ nav a {
 nav a:first-of-type {
   border: 0;
 }
-
-/*@media (min-width: 1024px) {*/
-/*  header {*/
-/*    display: flex;*/
-/*    place-items: center;*/
-/*    padding-right: calc(var(--section-gap) / 2);*/
-/*  }*/
-
-/*  .profile {*/
-/*    margin: 0 2rem 0 0;*/
-/*  }*/
-
-/*  header .wrapper {*/
-/*    display: flex;*/
-/*    place-items: flex-start;*/
-/*    flex-wrap: wrap;*/
-/*  }*/
-
-/*  nav {*/
-/*    text-align: left;*/
-/*    margin-left: -1rem;*/
-/*    font-size: 1rem;*/
-
-/*    padding: 1rem 0;*/
-/*    margin-top: 1rem;*/
-/*  }*/
-/*}*/
 </style>
