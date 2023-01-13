a5e5c857a5f30d70660fb03be56456571ae64f9a
Junyoung Yeom <yeomjy111@gmail.com>
2023-01-13 16:50:11 +0900

    update

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from "vue"
 import App from "./App.vue"
-import router from "./router"
 
 import "./assets/main.css"
 
@@ -16,7 +15,6 @@ const vuetify = createVuetify({
 })
 const app = createApp(App)
 
-app.use(router)
 app.use(vuetify)
 
 app.mount("#app")
