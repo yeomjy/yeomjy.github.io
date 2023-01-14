f207b05c37e5de0ebb99f8e5ffb596bb7c2298e1
Junyoung Yeom <yeomjy111@gmail.com>
2023-01-14 18:22:16 +0900

    update

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,11 @@
 <script setup lang="ts">
-import AboutView from "./views/MainView.vue"
+import MainView from "./views/MainView.vue"
 </script>
 
 <template>
   <v-app>
     <v-main>
-      <v-container>
-        <v-row dense>
-          <v-col>
-            <v-avatar size="125" class="profile">
-              <img
-                alt="Profile Image"
-                src="@/assets/profile.jpeg"
-                width="125"
-                height="125"
-              />
-            </v-avatar>
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col>
-            <h1 class="name">Junyoung Yeom</h1>
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col>
-            <div class="wrapper">
-              <nav>
-                <a href="https://github.com/yeomjy">GitHub</a>
-                <a href="mailto:yeomjy111@gmail.com">Email</a>
-                <a href="https://yeomjy.com/blog">Blog</a>
-              </nav>
-            </div>
-          </v-col>
-        </v-row>
-        <v-row>
-          <AboutView />
-        </v-row>
-      </v-container>
+      <MainView />
     </v-main>
     <v-footer>
       This page is built with &nbsp;
@@ -46,32 +14,3 @@ import AboutView from "./views/MainView.vue"
     </v-footer>
   </v-app>
 </template>
-
-<style scoped>
-.name {
-  text-align: center;
-}
-
-.profile {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-  background-color: transparent;
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-</style>
--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -2,10 +2,30 @@
 import Skills from "@/components/ProgrammingSkills.vue"
 import TA from "@/components/TeachingExperience.vue"
 import About from "@/components/MyInfo.vue"
+import Profile from "@/components/ProfileImage.vue"
 </script>
 <template>
   <div>
     <v-container>
+      <v-row>
+        <v-col>
+          <Profile />
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <h1 class="name">Junyoung Yeom</h1>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <nav>
+            <a href="https://github.com/yeomjy">GitHub</a>
+            <a href="mailto:yeomjy111@gmail.com">Email</a>
+            <a href="https://yeomjy.com/blog">Blog</a>
+          </nav>
+        </v-col>
+      </v-row>
       <v-row>
         <v-col>
           <About />
@@ -25,4 +45,26 @@ import About from "@/components/MyInfo.vue"
   </div>
 </template>
 
-<style></style>
+<style scoped>
+.name {
+  text-align: center;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+  background-color: transparent;
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+</style>
