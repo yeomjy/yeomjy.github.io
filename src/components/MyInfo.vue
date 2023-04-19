f6654ca10f1492016548838651143217a7e7617a
Junyoung Yeom <yeomjy111@gmail.com>
2023-04-19 10:37:29 +0900

    update

--- src/components/MyInfo.vue
+++ src/components/MyInfo.vue
@@ -1,11 +1,12 @@
 <template>
   <h2>About Me</h2>
   <p>
-    My name is Junyoung Yeom. I am an undergraduate student majoring in Computer
-    Science and Engineering, at Seoul National University. I am also double
-    majoring in mathematics. I am interested in Reinforcement Learning,
-    Machine Learning for Combinatorial Optimization(via RL), and Geometric Deep Learning.<br>
-    Currently, I am working as an undergraduate research intern at
+    My name is Junyoung Yeom. I'm an undergraduate student majoring in Computer
+    Science and Engineering. I'm also double
+    majoring in mathematics. I'm interested in Reinforcement Learning,
+    Machine Learning for Combinatorial Optimization(via RL), Self-Supervised Learning,
+    Causal Structure Learning, and Geometric Deep Learning.<br>
+    Currently, I'm working as an undergraduate research intern at
     <a href="https://mllab.snu.ac.kr">Machine Learning Lab</a>, Seoul National University.
   </p>
 </template>
