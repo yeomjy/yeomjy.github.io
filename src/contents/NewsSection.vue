94619a523131a4f3ab56259ccfc735ecb0327629
Junyoung Yeom <yeomjy@mllab.snu.ac.kr>
2024-01-19 20:24:34 +0900

    update

--- src/contents/NewsSection.vue
+++ src/contents/NewsSection.vue
@@ -1,17 +1,19 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 <template>
   <v-card flat>
     <v-card-title>
-      <h2 class="title font-weight-light mb-1 text-wrap">
-        News
-      </h2>
+      <h2 class="title font-weight-light mb-1 text-wrap">News</h2>
     </v-card-title>
     <v-card-text class="news-list-container">
       <ul>
         <li>
           <b>01/17/2024</b>
-          Our paper "Compressed Context Memory For Online Language Model Interaction"
+          Our paper
+          <a
+            href="https://janghyun1230.github.io/memory"
+            style="font-style: italic"
+            >Compressed Context Memory For Online Language Model Interaction</a
+          >
           is accepted at ICLR 2024!
         </li>
       </ul>
